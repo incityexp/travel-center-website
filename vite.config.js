7e3117150fed3959f2dda817d98b631f780356a8
Your Name <you@example.com>
2025-02-16 00:07:21 -0500

    Fixed index.html location

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // Ensures correct asset paths in Netlify
-  server: {
-    open: true,
-  },
+  publicDir: "public",  // Ensures `index.html` is used correctly
   build: {
-    outDir: "dist", // This ensures Netlify can use the correct folder
-  }
+    outDir: "dist",
+  },
 });
